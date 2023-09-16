606c9de8f0b57a36440232197dc228cfe2b3b457
rafaelswr <rafaels.borges91@gmail.com>
2023-09-16 15:47:27 +0100

    update initial test
    update test

--- src/main/java/rafaelswr/dev/movies/Controllers/AppController.java
+++ src/main/java/rafaelswr/dev/movies/Controllers/AppController.java
@@ -1,6 +1,5 @@
-package rafaelswr.dev.movies.controllers;
+package rafaelswr.dev.movies.Controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
